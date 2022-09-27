db228250bed33c63ca27571561e9c837cd07bd91
trosgate <isaacagbedam@gmail.com>
2022-09-27 18:28:14 +0000

    deployment phase two

--- trosgate/settings.py
+++ trosgate/settings.py
@@ -13,11 +13,12 @@ import os
 from pathlib import Path
 from django.contrib.messages import constants as messages
 from dotenv import load_dotenv
-
-load_dotenv()
-
+import dj_database_url
+# load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+load_dotenv(os.path.join(BASE_DIR, ".env"))
 
 
 # Quick-start development settings - unsuitable for production
@@ -147,25 +148,22 @@ WSGI_APPLICATION = 'trosgate.wsgi.application'
 
 
 # Database
-# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+DATABASES = {
+    "default": dj_database_url.config(default=DATABASE_URL, conn_max_age=1800),
+}
 
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'railway',
+#         'USER': 'postgres',
+#         'PASSWORD': '9vr1lrTivVSgmRxMa10I',
+#         'HOST': 'containers-us-west-77.railway.app',
+#         'PORT': '5718',
 #     }
 # }
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'railway',
-        'USER': 'postgres',
-        'PASSWORD': '9vr1lrTivVSgmRxMa10I',
-        'HOST': 'containers-us-west-77.railway.app',
-        'PORT': '5718',
-    }
-}
 # DATABASES = {
 #     'default': {
 #         'ENGINE': 'django.db.backends.postgresql',
