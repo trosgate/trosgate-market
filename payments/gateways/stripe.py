efcd32ca710ba16ad5ebc12585e3a532f04d7bcf
trosgate <isaacagbedam@gmail.com>
2023-03-17 19:29:17 +0000

    payment scenario

--- payments/gateways/stripe.py
+++ payments/gateways/stripe.py
@@ -1,5 +1,8 @@
 import stripe
+from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
 from payments.models import PaymentGateway
+
 
 class StripeCheckout:
     def __init__(self):
@@ -72,3 +75,30 @@ class StripeCheckout:
         except stripe.error.StripeError as e:
             # Handle any Stripe errors here
             return None
+    
+    @csrf_exempt
+    def handle_webhook(self, request):
+        payload = request.body
+        sig_header = request.META['HTTP_STRIPE_SIGNATURE']
+        endpoint_secret = 'your_endpoint_secret'  # replace with your webhook endpoint secret
+        
+        try:
+            event = stripe.Webhook.construct_event(
+                payload, sig_header, endpoint_secret
+            )
+        except ValueError as e:
+            # Invalid payload
+            return HttpResponse(status=400)
+        except stripe.error.SignatureVerificationError as e:
+            # Invalid signature
+            return HttpResponse(status=400)
+
+        # Handle the event
+        if event['type'] == 'checkout.session.completed':
+            session = event['data']['object']
+            # Handle completed checkout session
+            # ...
+        elif event['type'] == 'customer.subscription.created':
+            subscription = event['data']['object']
+            # Handle created subscription
+           
