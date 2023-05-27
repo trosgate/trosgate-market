e11c6e4c7c18db71791fa3a3b334c36d8b74f9d8
trosgate <isaacagbedam@gmail.com>
2023-05-27 17:33:21 +0000

    consumers

--- notification/consumers.py
+++ notification/consumers.py
@@ -1,4 +1,9 @@
+from channels.generic.websocket import WebsocketConsumer
 
 
-class NotifierConsumer():
-    pass
+class NotifierConsumer(WebsocketConsumer):
+    def connect(self):
+        self.accept()
+
+    def disconnect(self):
+        pass
--- trosgate/asgi.py
+++ trosgate/asgi.py
@@ -9,20 +9,20 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
 import os
 import django
 from channels.auth import AuthMiddlewareStack
+from channels.security.websocket import AllowedHostsOriginValidator
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
-from notification.routing import websocket_urlpatterns
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "trosgate.settings")
 django.setup()
 
+import notification.routing
+
 application = ProtocolTypeRouter({
   "http": get_asgi_application(),
-  "websocket": AuthMiddlewareStack(
-        URLRouter(
-            websocket_urlpatterns
-        )
-    ),
+  "websocket": AllowedHostsOriginValidator[
+      AuthMiddlewareStack(URLRouter(notification.routing.websocket_urlpatterns)),
+  ]
 })
 
 
